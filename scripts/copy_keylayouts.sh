3e5ac359b5e6fbc722cbd97dcbd753ae289d7914
sergey.chilingaryan <sergey.chilingaryan@talabat.com>
2023-11-14 02:06:59 +0400

    fix the workflow

--- scripts/copy_keylayouts.sh
+++ scripts/copy_keylayouts.sh
@@ -1,12 +1,3 @@
-# #!/bin/bash
-# # Navigate to the script's directory
-# cd "$(dirname "$0")"
-
-# # Copy keylayouts to the user's Keyboard Layouts directory
-# cp ../resources/*.keylayout ~/Library/Keyboard\ Layouts/
-# cp ../resources/*.icns ~/Library/Keyboard\ Layouts/
-
-
 #!/bin/bash
 # Navigate to the script's directory
 cd "$(dirname "$0")"
