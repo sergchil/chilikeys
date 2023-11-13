1140dcca1002c2118fe67f6ab1ccab282fb9bf92
sergey.chilingaryan <sergey.chilingaryan@talabat.com>
2023-11-14 02:06:59 +0400

    fix the workflow

--- scripts/bundle_all.sh
+++ scripts/bundle_all.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Navigate to the script's directory
+cd "$(dirname "$0")"
+
 # Define the source and destination directories
 KEY_LAYOUTS_DIR="../key_layouts"
-SCRIPT_DIR="../scripts/copy_keylayouts.sh"
-DEST_DIR="../bin/Install.app/Contents/Resources"
+SCRIPT_DIR="copy_keylayouts.sh"
+DEST_DIR="../bin/ChiliKeys.app/Contents/Resources"
 
 # Copy the files from the source to the destination
 cp -R "$KEY_LAYOUTS_DIR" "$DEST_DIR"
